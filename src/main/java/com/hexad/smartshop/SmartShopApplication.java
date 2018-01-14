99056da0a778bce5cc6e92469304d9288f8f4329
vijay <vijay@vijay-PC>
2018-01-14 21:37:49 +0530

    Creating develop branch

--- src/main/java/com/hexad/smartshop/SmartShopApplication.java
+++ src/main/java/com/hexad/smartshop/SmartShopApplication.java
@@ -18,28 +18,29 @@ import com.hexad.smartshop.test.data.OrderTestData;
 
 @SpringBootApplication
 public class SmartShopApplication {
-	
+
 	private final static Logger logger = LoggerFactory.getLogger(CustomerRegistration.class);
-	
+
 	public static void main(String[] args) throws Exception {
 		ApplicationContext context = SpringApplication.run(SmartShopApplication.class, args);
 		CustomerRegistration customerInit = context.getBean(CustomerRegistration.class);
-		/*** Uncomment below for new Customer Registration*/
-//		customerInit.registerCustomer(CustTestData.getCustomer());
-		/*** Uncomment below to update the existing Customer details*/
-//		customerInit.updateCustomer(CustTestData.getUpdateCustomer());
-		/*** Uncomment below to get existing Customer details by Id*/
-//		Customer customer=customerInit.getCustomerById(CustTestData.CUSTOMER_GET_ID);
-		
+		/*** Uncomment below for new Customer Registration */
+		// customerInit.registerCustomer(CustTestData.getCustomer());
+		/*** Uncomment below to update the existing Customer details */
+		// customerInit.updateCustomer(CustTestData.getUpdateCustomer());
+		/*** Uncomment below to get existing Customer details by Id */
+		// Customer customer=customerInit.getCustomerById(CustTestData.CUSTOMER_GET_ID);
+
 		Customer customer = CustTestData.getCustomer();
 		List<Product> productList = OrderTestData.getProductList(OrderTestData.productPriceMap);
 		Cart cart = OrderTestData.buildCart(customer, productList);
 		customer.setCart(cart);
 		Order orderMade = OrderTestData.buildOrder(customer);
-		logger.info("Order Details of : "+orderMade.getCustomer().getCustomerName());
-		logger.info("OrderId: "+orderMade.getOrderId()+" | OrderDate: "+orderMade.getDateOfOrder()+" | Current Status: "+orderMade.getCurrentStatus()+
-				" | Expected Delivery Date: "+orderMade.getDeliveryDate()+" | Order Price: "+orderMade.getTotalPrice());
+		logger.info("Order Details of : " + orderMade.getCustomer().getCustomerName());
+		logger.info("OrderId: " + orderMade.getOrderId() + " | OrderDate: " + orderMade.getDateOfOrder()
+				+ " | Current Status: " + orderMade.getCurrentStatus() + " | Expected Delivery Date: "
+				+ orderMade.getDeliveryDate() + " | Order Price: " + orderMade.getTotalPrice());
 
-//		SpringApplication.exit(context, () -> 0);
+		// SpringApplication.exit(context, () -> 0);
 	}
 }
